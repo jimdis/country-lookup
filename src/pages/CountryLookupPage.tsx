f6a4d3997ab58880971a5d5306fe82bb201f483e
jimdis <jim.disenstam@gmail.com>
2020-09-11 08:03:30 +0200

    Add hello world page

---
+++ src/pages/CountryLookupPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CountryLookupPage.module.css";
+
+const CountryLookupPage = () => {
+  return <div className={styles.root}>Hello world!</div>;
+};
+
+export default CountryLookupPage;
